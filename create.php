461061325dd96a44292f4c6a8e0b2338a5c2e2cb
Chandan <chandanrace0204@gmail.com>
2025-06-18 12:57:31 +0530

    Initial commit: Simple PHP CRUD project

---
+++ create.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head><title>Add User</title></head>
+<body>
+    <h2>Add User</h2>
+    <form method="POST" action="store.php">
+        Name: <input type="text" name="name" required><br><br>
+        Email: <input type="email" name="email" required><br><br>
+        <button type="submit">Save</button>
+    </form>
+</body>
+</html>
